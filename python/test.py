d3f053258928c8a3404409618ba9ea2f3f4cd243
Paul Wise <pabs3@bonedaddy.net>
2023-07-23 13:15:33 +0800

    Switch to print() function instead of print statement
    The print statement was removed for Python 3.
    
    Suggested-by: pyflakes & pylint

--- python/test.py
+++ python/test.py
@@ -7,42 +7,42 @@ ft.dwHighDateTime = 1
 
 
 for i in range(1, len(sys.argv)):
-    print "try file %s" % (sys.argv[i])
+    print("try file %s" % (sys.argv[i]))
     pst = _libpst.pst(sys.argv[i], "")
     topf = pst.pst_getTopOfFolders()
 
-    print pst.pst_rfc2425_datetime_format(ft)
-    print pst.pst_rfc2445_datetime_format(ft)
+    print(pst.pst_rfc2425_datetime_format(ft))
+    print(pst.pst_rfc2445_datetime_format(ft))
 
     while (topf):
-        print "topf d_id is %d\n" % (topf.d_id)
+        print("topf d_id is %d\n" % (topf.d_id))
         item = pst.pst_parse_item(topf, None)
         if (item):
             if (item.type == 1):
                 em = item.email
                 if (em):
                     default_charset = pst.pst_default_charset(item)
-                    print "default charset %s" % (default_charset)
+                    print("default charset %s" % (default_charset))
                     ft = em.arrival_date
                     if (ft):
                         ft = pst.pst_rfc2425_datetime_format(ft)
-                        print "message arrived at %s" % (ft)
+                        print("message arrived at %s" % (ft))
                     if (em.messageid.str):
-                        print "message id is <%s>" % (em.messageid.str)
+                        print("message id is <%s>" % (em.messageid.str))
                     subj = item.subject
                     rtf = em.rtf_compressed
                     if rtf and (len(rtf) > 0):
-                        print "rtf compressed size", len(rtf)
+                        print("rtf compressed size", len(rtf))
                     if (subj and subj.str):
                         was = subj.is_utf8
                         pst.pst_convert_utf8(item, subj)
                         now = subj.is_utf8
                         if (was != now):
-                            print "subject was converted to utf8"
-                        print "subject is %s" % (subj.str)
+                            print("subject was converted to utf8")
+                        print("subject is %s" % (subj.str))
                     body = item.body
                     if (body.str):
-                        print "message body is %s" % (body.str)
+                        print("message body is %s" % (body.str))
                     att = item.attach
                     att = None
                     while (att):
@@ -50,24 +50,24 @@ for i in range(1, len(sys.argv)):
                         att1 = att.filename1
                         att2 = att.filename2
                         msg = "attachment id %d file name short '%s' long '%s'"
-                        print msg % (attid, att1.str, att2.str)
+                        print(msg % (attid, att1.str, att2.str))
                         if (1):
                             attdata = pst.pst_attach_to_mem(att)
                             if (attdata):
-                                print "data size %d" % (len(attdata))
+                                print("data size %d" % (len(attdata)))
                         if (0):
                             f = pst.ppst_open_file(att2.str, 'w')
                             if (f):
                                 si = pst.pst_attach_to_file_base64(att, f)
                                 pst.ppst_close_file(f)
-                                print "wrote %d bytes in %s" % (si, att2.str)
+                                print("wrote %d bytes in %s" % (si, att2.str))
                         if (0):
                             f = pst.ppst_open_file(att2.str, 'w')
                             if (f):
                                 si = pst.pst_attach_to_file(att, f)
                                 pst.ppst_close_file(f)
-                                print "wrote %d bytes in %s" % (si, att2.str)
+                                print("wrote %d bytes in %s" % (si, att2.str))
                         att = att.next
             pst.pst_freeItem(item)
         topf = pst.pst_getNextDptr(topf)
-    print "done"
+    print("done")
